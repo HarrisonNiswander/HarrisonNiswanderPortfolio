137fb64cf5962251beef6b8fb205e5e95a760c15
Harrison Niswander <niswhd01@pfw.edu>
2025-12-03 23:04:06 -0500

    Create about page

--- HarrisonNiswanderPortfolio/src/main.jsx
+++ HarrisonNiswanderPortfolio/src/main.jsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home-Page/homePage.jsx";
+import About from "./pages/About/about.jsx";
 
 const router = createBrowserRouter([
   {
     //Home Page Route
-    path: "/",
+    path: "/home",
     element: <HomePage />
+  }, 
+
+  {
+    //About Page Route
+    path: "/about",
+    element: <About />
   }
   
 ]);
---
+++ HarrisonNiswanderPortfolio/src/pages/About/about.jsx
@@ -0,0 +1,8 @@
+const about = () => (
+  <div>
+    <h2>About Page</h2>
+  </div>
+
+)
+
+export default about;
