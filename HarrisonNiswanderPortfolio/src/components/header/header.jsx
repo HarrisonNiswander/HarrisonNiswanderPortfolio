7bc1c7c59a1b4592fd3298384893d997346f8034
Harrison Niswander <niswhd01@pfw.edu>
2025-12-05 01:37:20 -0500

    Create header for every page & started styling

---
+++ HarrisonNiswanderPortfolio/src/components/header/header.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import './header_style/header.css';
+import HN_Signature from '../../images/HN_Signature.png';
+
+export default function header() {
+    return (
+        <>
+            {/*Navigation Menu*/}
+            <header>
+                
+
+            </header>
+            
+            {/*Navigation Menu*/}
+            <nav class="container">
+                <ul>
+                    {/*Image*/}
+                    <Link to="/home">
+                        <img src={HN_Signature} alt="Harrison Niswander Signature" height={50} width={130}/>
+                    </Link>
+
+                    <Link to="/home">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/portfolio">Portfolio</Link>
+                    <Link to="/portfolio">Contact</Link>
+
+                </ul>
+
+            </nav>
+
+        </>
+
+    )
+}
--- HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
@@ -1,7 +1,10 @@
-import "./home.css";
+import "./home_styles/home.css";
+import Header from '../../components/header/header.jsx';
 
 const homePage = () => (
   <div>
+    <Header />
+
     <h2>Home Page</h2>
   </div>
 
