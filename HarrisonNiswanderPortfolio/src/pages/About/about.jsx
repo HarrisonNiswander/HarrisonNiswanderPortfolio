56bc80f3235219e98606c406c83be62af4d45e01
Harrison Niswander <niswhd01@pfw.edu>
2025-12-06 00:42:03 -0500

    Add footer to about page

--- HarrisonNiswanderPortfolio/src/pages/About/about.jsx
+++ HarrisonNiswanderPortfolio/src/pages/About/about.jsx
@@ -1,10 +1,13 @@
 import Header from '../../components/header/header.jsx';
+import Footer from '../../components/footer/footer.jsx';
 
 const about = () => (
   <div>
     <Header />
     
     <h2>About Page</h2>
+
+    <Footer />
   </div>
 
 )
