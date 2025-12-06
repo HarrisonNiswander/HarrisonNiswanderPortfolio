8cda04fa5f5b07ed027f4e349bd7c55ed9d6ecdf
Harrison Niswander <niswhd01@pfw.edu>
2025-12-06 00:43:13 -0500

    Add footer to contact page

--- HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
@@ -1,10 +1,13 @@
 import Header from '../../components/header/header.jsx';
+import Footer from '../../components/footer/footer.jsx';
 
 const contact = () => (
   <div>
     <Header />
     
     <h2>Contact Page</h2>
+
+    <Footer />
   </div>
 
 )
