c73255fc6bdb2151591b8003fbdee209a34cf1a4
Harrison Niswander <niswhd01@pfw.edu>
2025-12-06 00:45:43 -0500

    Add footer to portfolio page

--- HarrisonNiswanderPortfolio/src/pages/Portfolio/portfolio.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Portfolio/portfolio.jsx
@@ -1,10 +1,13 @@
 import Header from '../../components/header/header.jsx';
+import Footer from '../../components/footer/footer.jsx';
 
 const portfolio = () => (
   <div>
     <Header />
     
     <h2>Portfolio Page</h2>
+
+    <Footer />  
   </div>
 
 )
