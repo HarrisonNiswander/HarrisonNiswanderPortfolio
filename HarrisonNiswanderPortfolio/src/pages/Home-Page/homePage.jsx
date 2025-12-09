637bc752542b00ae57c7a4808c9602f8b7221ec7
Harrison Niswander <niswhd01@pfw.edu>
2025-12-09 14:06:56 -0500

    Add div components to home-page w/ hover features. Also fixed footer so it stays at bottom of page

--- HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
@@ -1,27 +1,70 @@
 import "./home_styles/home.css";
 import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
+import { Link } from "react-router-dom";
 
 const homePage = () => (
   <div>
     <Header />
 
-    <h2>Home Page</h2>
+    {/* Header Section */}
+    <header>
+      <h1>Welcome to Harrison Niswander's Portfolio!</h1>
+
+      <section class="intro">
+        <h3>
+          Here, you'll find a showcase of my projects, skills, and experiences as a developer. Feel free to 
+          explore the different sections to learn more about me and my work. If you have any questions or 
+          would like to get in touch, don't hesitate to reach out through the contact page. 
+        </h3>
+      </section>
+
+      <br/>{/* Break */}
+
+    </header>
     
-    <br/><br/><br/>
+    {/* Different Pages Section */}
+    <main>
+      <div class="outermost-container">
+        <ul class="row1">
+          <div class="item">
+            <h1>About Page</h1>
 
-    <p>
-      Welcome to my portfolio website! Here, you'll find a showcase of my projects, skills, and 
-      experiences as a developer. Feel free to explore the different sections to learn more about me 
-      and my work. If you have any questions or would like to get in touch, don't hesitate to reach out 
-      through the contact page. Thank you for visiting!
-    </p>
+          </div>  {/* About Page */}
 
-    <br/><br/><br/><br/>
+          <div class="item">
+            <h1>Portfolio Page</h1>
 
+          </div>  {/* Portfolio Page */}
+
+        </ul>  {/* Row 1 */}
+
+        <br/>{/* Break */}
+
+        <ul class="row2">
+          <div class="item">
+            <h1>Contact Page</h1>
+
+          </div>  {/* Contact Page */}
+
+          <div class="item">
+            <h1>Lime City Ultimate</h1>
+
+          </div>  {/* Lime City Ultimate Page */}
+
+        </ul>  {/* Row 2 */}
+
+      </div>  {/* Outer Most Container */}
+
+    </main>
+
+    <br/><br/><br/><br/> {/* Break */}
 
     <Footer/>
+
   </div>
+
+  
 
 )
 
