cd7b26458be96b1afc7de3ae1a60c186a7c25483
Harrison Niswander <niswhd01@pfw.edu>
2025-12-09 20:58:31 -0500

    Updated graphics in the homepage and redid the design/color scheme

--- HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Home-Page/homePage.jsx
@@ -2,70 +2,96 @@ import "./home_styles/home.css";
 import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import HN_Signature from '../../images/HN_Signature.png';
 
-const homePage = () => (
-  <div>
-    <Header />
 
-    {/* Header Section */}
-    <header>
-      <h1>Welcome to Harrison Niswander's Portfolio!</h1>
 
-      <section class="intro">
-        <h3>
-          Here, you'll find a showcase of my projects, skills, and experiences as a developer. Feel free to 
-          explore the different sections to learn more about me and my work. If you have any questions or 
-          would like to get in touch, don't hesitate to reach out through the contact page. 
-        </h3>
-      </section>
+const homePage = () => {
+  const navigate = useNavigate();
 
-      <br/>{/* Break */}
+  const aboutPageClick = () => {
+    navigate('/about'); 
+  };
 
-    </header>
-    
-    {/* Different Pages Section */}
-    <main>
-      <div class="outermost-container">
-        <ul class="row1">
-          <div class="item">
-            <h1>About Page</h1>
+  const portfolioPageClick = () => {
+    navigate('/portfolio'); 
+  };
 
-          </div>  {/* About Page */}
+  const contactPageClick = () => {
+    navigate('/contact'); 
+  };
 
-          <div class="item">
-            <h1>Portfolio Page</h1>
+  /*
+  const ultimatePageClick = () => {
+    navigate('/lime-city-ultimate'); 
+  };
+  */
 
-          </div>  {/* Portfolio Page */}
+  return (
+    <div>
+      <Header />
 
-        </ul>  {/* Row 1 */}
+      {/* Header Section */}
+      <header>
+        <h1>Welcome to Harrison Niswander's Portfolio!</h1>
+
+        <section class="intro">
+          <h3>
+            Here, you'll find a showcase of my projects, skills, and experiences as a developer. Feel free to 
+            explore the different sections to learn more about me and my work. If you have any questions or 
+            would like to get in touch, don't hesitate to reach out through the contact page. 
+          </h3>
+        </section>
 
         <br/>{/* Break */}
 
-        <ul class="row2">
-          <div class="item">
-            <h1>Contact Page</h1>
+      </header>
+      
+      {/* Different Pages Section */}
+      <main>
+        <div class="outermost-container">
+          <ul class="row1">
+            <a class="about" onClick={aboutPageClick}>
+              <h1>About Me</h1>
 
-          </div>  {/* Contact Page */}
+            </a>  {/* About Page */}
 
-          <div class="item">
-            <h1>Lime City Ultimate</h1>
+            <div class="port" onClick={portfolioPageClick}>
+              <h1>Portfolio</h1>
 
-          </div>  {/* Lime City Ultimate Page */}
+            </div>  {/* Portfolio Page */}
 
-        </ul>  {/* Row 2 */}
+          </ul>  {/* Row 1 */}
 
-      </div>  {/* Outer Most Container */}
+          <br/>{/* Break */}
 
-    </main>
+          <ul class="row2">
+            <div class="contact" onClick={contactPageClick}>
+              <h1>Contact Me</h1>
 
-    <br/><br/><br/><br/> {/* Break */}
+            </div>  {/* Contact Page */}
 
-    <Footer/>
+            <div class="ultimate">
+              <h1>Lime City<br/>Ultimate</h1>
 
-  </div>
+            </div>  {/* Lime City Ultimate Page */}
 
+          </ul>  {/* Row 2 */}
+
+        </div>  {/* Outer Most Container */}
+
+      </main>
+
+      <br/><br/><br/><br/> {/* Break */}
+
+      <Footer/>
+
+    </div>
+
+  );
   
 
-)
+}
 
 export default homePage;
