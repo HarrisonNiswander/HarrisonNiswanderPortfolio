4a95c6ed637a2a6aa6bea6aaccd6bfc126b4bdb5
Harrison Niswander <niswhd01@pfw.edu>
2025-12-11 12:51:55 -0500

    Initial creation of icons on contact page

--- HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
@@ -1,11 +1,43 @@
+import "./contact_styles/contact.css";
 import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
+
+//images
+import facebook from '../../images/contact-page/facebook.png';
+import gmail from '../../images/contact-page/gmail.png';
+import instagram from '../../images/contact-page/instagram.png';
+import linkedin from '../../images/contact-page/linkedin.png';
+import strava from '../../images/contact-page/strava.png';
+import twitter from '../../images/contact-page/twitter.png';
+import github from '../../images/contact-page/github.png';
+import garmin from '../../images/contact-page/garmin.png';
+
 
 const contact = () => (
   <div>
     <Header />
     
-    <h2>Contact Page</h2>
+    <h1 className="contact-font">Contact Page</h1>
+
+    {/* Contact Links Section */}
+    <main className="links">
+      <ul className="row">
+        <img src={gmail} alt="Gmail" height={200} width={200} className="icon"/>
+        <img src={linkedin} alt="LinkedIn" height={200} width={200} className="icon"/>
+        <img src={github} alt="GitHub" height={200} width={200} className="icon"/>
+        <img src={strava} alt="Strava" height={200} width={200} className="icon"/>
+      </ul>
+
+      <ul className="row">
+        <img src={instagram} alt="Instagram" height={200} width={200} className="icon"/>
+        <img src={facebook} alt="Facebook" height={215} width={215} className="icon"/>
+        <img src={twitter} alt="Twitter" height={200} width={200} className="icon"/>
+        <img src={garmin} alt="Garmin" height={200} width={200} className="icon"/>
+      </ul>
+
+    </main>
+
+   
 
     <Footer />
   </div>
