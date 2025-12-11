e6e0066c3201000e1f45ec8a21a0776c3de77eb9
Harrison Niswander <niswhd01@pfw.edu>
2025-12-11 14:16:57 -0500

    Update links to contact page

--- HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Contact/contact.jsx
@@ -22,17 +22,39 @@ const contact = () => (
     {/* Contact Links Section */}
     <main className="links">
       <ul className="row">
-        <img src={gmail} alt="Gmail" height={200} width={200} className="icon"/>
-        <img src={linkedin} alt="LinkedIn" height={200} width={200} className="icon"/>
-        <img src={github} alt="GitHub" height={200} width={200} className="icon"/>
-        <img src={strava} alt="Strava" height={200} width={200} className="icon"/>
+        <a href="mailto:harrisonniswander@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src={gmail} alt="Gmail" height={200} width={200} className="icon"/>
+        </a>
+
+        <a href="https://www.linkedin.com/in/harrison-niswander-99267b327/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin} alt="LinkedIn" height={200} width={200} className="icon"/>
+        </a>
+
+        <a href="https://github.com/HarrisonNiswander" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="GitHub" height={200} width={200} className="icon"/>
+        </a>
+
+        <a href="https://www.strava.com/athletes/43329926" target="_blank" rel="noopener noreferrer">
+          <img src={strava} alt="Strava" height={200} width={200} className="icon"/>
+        </a>
       </ul>
 
       <ul className="row">
-        <img src={instagram} alt="Instagram" height={200} width={200} className="icon"/>
-        <img src={facebook} alt="Facebook" height={215} width={215} className="icon"/>
-        <img src={twitter} alt="Twitter" height={200} width={200} className="icon"/>
-        <img src={garmin} alt="Garmin" height={200} width={200} className="icon"/>
+        <a href="https://www.instagram.com/hniswander03/" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="Instagram" height={200} width={200} className="icon"/>
+        </a>
+
+        <a href="https://www.facebook.com/harrison.niswander/" target="_blank" rel="noopener noreferrer">
+          <img src={facebook} alt="Facebook" height={215} width={215} className="icon"/>
+        </a>
+
+        <a href="https://x.com/HarrisonNiswan1" target="_blank" rel="noopener noreferrer">
+          <img src={twitter} alt="Twitter" height={200} width={200} className="icon"/>
+        </a>
+
+        <a href="https://connect.garmin.com/modern/profile/04c3a507-ab31-4651-8c83-da5c5aefa07e" target="_blank" rel="noopener noreferrer">
+          <img src={garmin} alt="Garmin" height={200} width={200} className="icon"/>
+        </a>
       </ul>
 
     </main>
