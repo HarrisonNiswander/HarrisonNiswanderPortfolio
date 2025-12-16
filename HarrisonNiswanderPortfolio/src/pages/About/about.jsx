9287c7e4a2e00a9ac48660c27063a9bafce70774
Harrison Niswander <niswhd01@pfw.edu>
2025-12-15 20:30:39 -0500

    Work on formatting tabs

--- HarrisonNiswanderPortfolio/src/pages/About/about.jsx
+++ HarrisonNiswanderPortfolio/src/pages/About/about.jsx
@@ -22,31 +22,35 @@ const about = () => (
   
   <div>
 
-    
     <Header />
     
     <h1>About Page</h1>
 
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
-        <Col sm={3}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Tab 1</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">Tab 2</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col sm={9}>
-          <Tab.Content>
-            <Tab.Pane eventKey="first">First tab content</Tab.Pane>
-            <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+    <div className="profile">
+      <Tab.Container id="left-tabs" defaultActiveKey="first">
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item><Nav.Link eventKey="first">Academics</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link eventKey="second">Athletics</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link eventKey="third">Work Experience</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link eventKey="fourth">Volunteer Work</Nav.Link></Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content className="tab-content">
+              <Tab.Pane eventKey="first">Academics</Tab.Pane>
+              <Tab.Pane eventKey="second">Athletics</Tab.Pane>
+              <Tab.Pane eventKey="third">Work Experience</Tab.Pane>
+              <Tab.Pane eventKey="fourth">Volunteer Work</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+
+
+
+    </div>
 
     <Footer />
   </div>
