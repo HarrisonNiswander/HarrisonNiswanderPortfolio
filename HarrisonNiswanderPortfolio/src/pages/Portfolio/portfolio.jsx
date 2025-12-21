2fbb3d1066bdae8aa0768a61ba92f8d6efc02794
Harrison Niswander <niswhd01@pfw.edu>
2025-12-20 20:52:07 -0500

    Work on portfolio page

--- HarrisonNiswanderPortfolio/src/pages/Portfolio/portfolio.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Portfolio/portfolio.jsx
@@ -1,15 +1,34 @@
+import "./portfolio_styles/portfolio.css";
+import { useState } from "react";
 import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
+import PortfolioGrid from "../../components/portfolio-menu/portfolio-grid.jsx";
 
-const portfolio = () => (
-  <div>
-    <Header />
-    
-    <h2>Portfolio Page</h2>
 
-    <Footer />  
-  </div>
+const games = [
+  { id: 1, name: "Signing Bee", image: "./images/signingBeeHome.png" },
+  { id: 2, name: "Island Song", image: "./images/islandSongLogo.png" },
+  { id: 3, name: "Blank", image: "./images/signingBeeHome.png" },
+  { id: 4, name: "Blank", image: "./images/signingBeePlay.png" },
 
-)
+];
+
+
+const portfolio = () => {
+  const [selectedGame, setSelectedGame] = useState(null);
+
+  return (
+    <div>
+      <Header />
+      
+      <h2>Portfolio Page</h2>
+
+      
+
+      <Footer />  
+    </div>
+
+  );
+}
 
 export default portfolio;
