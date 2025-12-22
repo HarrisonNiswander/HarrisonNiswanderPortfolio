3c7ec7d7975f6e9fdaaac7f9631266577052baf5
Harrison Niswander <niswhd01@pfw.edu>
2025-12-21 22:24:36 -0500

    Added content to signing bee page (creators and title at top)

--- HarrisonNiswanderPortfolio/src/pages/Portfolio/Signing-Bee/signing-bee.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Portfolio/Signing-Bee/signing-bee.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
+import "./signing-bee_styles/signing-bee.css";
 import Header from '../../../components/header/header.jsx';
 import Footer from '../../../components/footer/footer.jsx';
 import signingBeeLogo from '../../../images/portfolio-page/Signing-Bee/signingBeeLogo.png';
 import signingBeeHome from '../../../images/portfolio-page/Signing-Bee/signingBeeHome.png';
 import signingBeePlay from '../../../images/portfolio-page/Signing-Bee/signingBeePlay.png';
+import signingBeeLogoSVG from '../../../images/portfolio-page/Signing-Bee/signing-bee-logo.svg';
 import ImageCarousel from '../../../components/image-carousel/image-carousel.jsx';
+import Steven from '../../../images/portfolio-page/Signing-Bee/creators/steven_picture.png';
+import Harrison from '../../../images/portfolio-page/Signing-Bee/creators/harrison_picture.jpg';
+import Ricardo from '../../../images/portfolio-page/Signing-Bee/creators/ricardo_picture.png';
+import Zack from '../../../images/portfolio-page/Signing-Bee/creators/zack_picture.jpg';
 
 const carouselImages = [
     { src: signingBeeLogo, description: 'Signing Bee Logo' },
@@ -17,10 +23,43 @@ const signingBee = () => {
   return (
     <div>
         <Header />
-
-        <div className="App">
+        <div className="sb-title">
             <h1>Signing Bee</h1>
+            <img src={signingBeeLogoSVG} width="50" height="50" />
+        </div>
+
+        <div className="carousel">
             <ImageCarousel images={carouselImages} interval={4000} />
+
+        </div>
+
+        <h2>Meet the Creators</h2>
+
+        <div className="creators-container">
+            
+            <div className="creator-card">
+                <img src={Harrison} alt="Harrison Niswander" height={200} width={200} className="creator-image"/>
+                <h3 className="name">Harrison Niswander</h3>
+
+            </div>
+
+            <div className="creator-card">
+                <img src={Ricardo} alt="Ricardo Saldana" height={200} width={200} className="creator-image"/>
+                <h3 className="name">Ricardo Saldana</h3>
+
+            </div>
+
+            <div className="creator-card">
+                <img src={Steven} alt="Steven LaGoy" height={200} width={200} className="creator-image"/>
+                <h3 className="name">Steven LaGoy</h3>
+                
+            </div>
+
+            <div className="creator-card">
+                <img src={Zack} alt="Zachary McGill" height={200} width={200} className="creator-image"/>
+                <h3 className="name">Zachary McGill</h3>
+                
+            </div>
 
         </div>
 
