02453df369208e4752cb75a3f287639bdebc4c45
Harrison Niswander <niswhd01@pfw.edu>
2025-12-22 17:29:59 -0500

    Finished formatting for signing bee page

--- HarrisonNiswanderPortfolio/src/pages/Portfolio/Signing-Bee/signing-bee.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Portfolio/Signing-Bee/signing-bee.jsx
@@ -10,6 +10,7 @@ import signingBeeLogin from '../../../images/portfolio-page/Signing-Bee/signingB
 import signingBeeASL from '../../../images/portfolio-page/Signing-Bee/signingBeeASLRef.png';
 import signingBeeLogoSVG from '../../../images/portfolio-page/Signing-Bee/signing-bee-logo.svg';
 import ImageCarousel from '../../../components/image-carousel/image-carousel.jsx';
+import githubBlack from '../../../images/portfolio-page/Signing-Bee/githubBlack.png';
 import Steven from '../../../images/portfolio-page/Signing-Bee/creators/steven_picture.png';
 import Harrison from '../../../images/portfolio-page/Signing-Bee/creators/harrison_picture.jpg';
 import Ricardo from '../../../images/portfolio-page/Signing-Bee/creators/ricardo_picture.png';
@@ -41,77 +42,116 @@ const signingBee = () => {
             <img src={signingBeeLogoSVG} width="50" height="50" />
         </div>
 
+        <br/>
+
         <div className="carousel">
             <ImageCarousel images={carouselImages} interval={4000} />
 
         </div>
 
-        <h2>About the Project</h2>
+        <br/>
 
-        <p>
-            Signing Bee was made in Fall 2025 at Purdue University Fort Wayne as a semester-long 
-            project for CS 347: Full-Stack Web Development. Signing Bee is a learning platform 
-            designed to teach the basics of ASL spelling through an interactive game platform.
+        <div className="sb-about">
+            <h2>About the Project</h2>
 
-            <br/><br/>
+            <p>
+                Signing Bee was made in Fall 2025 at Purdue University Fort Wayne as a semester-long 
+                project for CS 347: Full-Stack Web Development. Signing Bee is a learning platform 
+                designed to teach the basics of ASL spelling through an interactive game platform.
 
-            Signing Bee was made over the course of 16 weeks, starting with basic prototypes of the 
-            gesture recognition system, constructing a website around that, and then connecting the 
-            page to other server-side services keep the leaderboard. 
-        </p>
+                <br/><br/>
 
-        <h2>Tech Stack</h2>
-
-        <ul>
-            {/* List of technologies used in the project */}
-            <li>Frontend: React, HTML, CSS</li>
-            <li>Backend: Node.js, Express.js</li>
-            <li>Database: PostgreSQL on Supabase</li>
-            <li>Testing: Cypress (E2E), Jest, Supertest</li>
-            <li>CI/CD: GitHub Actions</li>
-            <li>Hosting: Google Cloud platform</li>
-
-            {/* PUT THIS IN TABLE */}
-            {/* PUT THIS IN TABLE */}
-            {/* PUT THIS IN TABLE */}
-            {/* PUT THIS IN TABLE */}
-            {/* PUT THIS IN TABLE */}
-        </ul>
-
-        <h2>Meet the Creators</h2>
-
-        <div className="creators-container">
-            
-            <div className="creator-card">
-                <img src={Harrison} alt="Harrison Niswander" height={200} width={200} className="creator-image"/>
-                <h3 className="name">Harrison Niswander</h3>
-
-            </div>
-
-            <div className="creator-card">
-                <img src={Ricardo} alt="Ricardo Saldana" height={200} width={200} className="creator-image"/>
-                <h3 className="name">Ricardo Saldana</h3>
-
-            </div>
-
-            <div className="creator-card">
-                <img src={Steven} alt="Steven LaGoy" height={200} width={200} className="creator-image"/>
-                <h3 className="name">Steven LaGoy</h3>
-                
-            </div>
-
-            <div className="creator-card">
-                <img src={Zack} alt="Zachary McGill" height={200} width={200} className="creator-image"/>
-                <h3 className="name">Zachary McGill</h3>
-                
-            </div>
+                Signing Bee was made over the course of 16 weeks, starting with basic prototypes of the 
+                gesture recognition system, constructing a website around that, and then connecting the 
+                page to other server-side services to keep the leaderboard. 
+            </p>
 
         </div>
 
-        <br/>
+        <br/><br/>
 
-        <h2>Github Repository</h2>
-        <p><a href="https://github.com/stevenlagoy/Signing-Bee">Signing Bee on GitHub</a></p>
+        <div className="tech-stack">
+            <h2>Tech Stack</h2>
+        
+            {/* List of technologies used in the project */}
+            <table className="tech-table">
+                <tr>
+                    <td className="tech">Frontend</td>
+                    <td className="used">React, HTML,  & CSS</td>
+                </tr>
+
+                <tr>
+                    <td className="tech">Backend</td>
+                    <td className="used">Node.js & Express.js</td>
+                </tr>
+
+                <tr>
+                    <td className="tech">Database</td>
+                    <td className="used">PostgreSQL on Supabase</td>
+                </tr>
+
+                <tr>
+                    <td className="tech">Testing</td>
+                    <td className="used">Cypress (E2E), Jest, & Supertest</td>
+                </tr>
+
+                <tr>
+                    <td className="tech">CI/CD</td>
+                    <td className="used">Github Actions</td>
+                </tr>
+
+                <tr>
+                    <td className="tech">Hosting</td>
+                    <td className="used">Google Cloud Platform</td>
+                </tr>
+
+            </table>
+        </div>
+
+        <br/><br/>
+
+        <div className="creators-background">
+            <h2>Meet the Creators</h2>
+
+            <div className="creators-container">
+                
+                <div className="creator-card">
+                    <img src={Harrison} alt="Harrison Niswander" height={200} width={200} className="creator-image"/>
+                    <h3 className="name">Harrison Niswander</h3>
+
+                </div>
+
+                <div className="creator-card">
+                    <img src={Ricardo} alt="Ricardo Saldana" height={200} width={200} className="creator-image"/>
+                    <h3 className="name">Ricardo Saldana</h3>
+
+                </div>
+
+                <div className="creator-card">
+                    <img src={Steven} alt="Steven LaGoy" height={200} width={200} className="creator-image"/>
+                    <h3 className="name">Steven LaGoy</h3>
+                    
+                </div>
+
+                <div className="creator-card">
+                    <img src={Zack} alt="Zachary McGill" height={200} width={200} className="creator-image"/>
+                    <h3 className="name">Zachary McGill</h3>
+                    
+                </div>
+
+            </div>
+        </div>
+
+        <br/><br/>
+
+        <div className="github">
+            <h2>Github Repository</h2>
+            <img src={githubBlack} alt="GitHub Logo" height={150} width={400} className="github-logo"
+                onClick={() => window.open("https://github.com/stevenlagoy/Signing-Bee", "_blank")}
+            />
+        </div>
+
+        <br/><br/><br/>
 
         <Footer />
     </div>
