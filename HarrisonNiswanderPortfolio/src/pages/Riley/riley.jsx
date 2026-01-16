fea65d946edc82cb0230686cd1722c461144bab2
Harrison Niswander <niswhd01@pfw.edu>
2026-01-16 15:51:43 -0500

    created vertical image carousel for vertical images and added some initial pictures for the plan

--- HarrisonNiswanderPortfolio/src/pages/Riley/riley.jsx
+++ HarrisonNiswanderPortfolio/src/pages/Riley/riley.jsx
@@ -1,14 +1,47 @@
 import "./riley_styles/riley.css";
 import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
+import ImageCarouselVert from '../../components/image-carousel-vert/image-carousel-vert.jsx';
 
-const contact = () => (
-  <div>
-    <Header />
+//images
+import plan1 from '../../images/riley-page/plan/plan1.jpeg';
+import plan2 from '../../images/riley-page/plan/plan2.jpeg';
+import plan3 from '../../images/riley-page/plan/plan3.jpeg';
+import plan4 from '../../images/riley-page/plan/plan4.jpeg';
+import plan5 from '../../images/riley-page/plan/plan5.jpeg';
+
+const carouselPlan = [
+    { src: plan1, description: 'Image #1 for plan' },
+    { src: plan2, description: 'Image #2 for plan' },
+    { src: plan3, description: 'Image #3 for plan' },
+    { src: plan4, description: 'Image #4 for plan' },
+    { src: plan5, description: 'Image #5 for plan' },
+];
+
+const riley = () => {
     
-    <Footer />
-      </div>
+    return (
+    <div>
+        <Header />
+
+        <h1>Happy 2 Years Riley: Wooden Calendar</h1>
+
+        <br/>
+
+        <h2>The Initial Plan</h2>
+
+        <div className="carousel">
+            <ImageCarouselVert images={carouselPlan} interval={4000} className="plan-carousel"/>
+        </div>
+
+        <br/>
+        
+        <Footer />
+
+    </div>
+
+    );
     
-)
+}
     
-export default contact;
+export default riley;
